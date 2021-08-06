c8b155fe3e144ca7ef9a3469c8d7b2207ebb850a
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2021-08-06 18:12:09 +0900

    Update forgotPassword.js

--- src/pages/forgotPassword.js
+++ src/pages/forgotPassword.js
@@ -1,73 +1,80 @@
-import { useContext, useState } from 'react';
+import { useContext, useState } from "react";
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import { useHistory } from "react-router";
 
-import * as ROUTES from '../constants/routes';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-import FirebaseContext from '../context/firebase';
+import * as ROUTES from "../constants/routes";
+
+import FirebaseContext from "../context/firebase";
 
 // for UI
 
-  function Copyright() {
+function Copyright() {
     return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://www.jg-corporation.com/">
-          JG Corporation
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://www.jg-corporation.com/">
+                JG Corporation
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
     );
-  };
+}
 
-  const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
     paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
     },
     form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
     },
     submit: {
-      margin: theme.spacing(3, 0, 2),
+        margin: theme.spacing(3, 0, 2),
     },
-  }));
+}));
 
 // end ui components
 
 export default function ForgotPassword() {
-
     const classes = useStyles();
+    const history = useHistory();
 
     const { firebase } = useContext(FirebaseContext);
 
-    const [emailAddress, setEmailAddress] = useState('');
-    const [pwResetEmail, setPwResetEmail] = useState('');
-    const [error, setError] = useState('');
+    const [emailAddress, setEmailAddress] = useState("");
+    const [pwResetEmail, setPwResetEmail] = useState("");
+    const [error, setError] = useState("");
 
-    const disabled = !emailAddress
+    const disabled = !emailAddress;
 
     const handleResetPassword = async (e) => {
         e.preventDefault();
-        try{
+        try {
             await firebase.auth().sendPasswordResetEmail(emailAddress);
-            setPwResetEmail('Password Reset Sent! Check your email');
-            setEmailAddress('');
-        } catch(e) {
-            setError(e.message)
+            setPwResetEmail(
+                "Password Reset Sent! Check your email. Redirecting you to login..."
+            );
+            setEmailAddress("");
+            setTimeout(() => {
+                history.push(ROUTES.LOGIN);
+            }, 3000);
+        } catch (e) {
+            setError(e.message);
         }
-    }
+    };
 
     return (
         <Container component="main" maxWidth="xs">
@@ -75,28 +82,37 @@ export default function ForgotPassword() {
                 <Typography component="h1" variant="h5">
                     Did You Forget Your Password?
                 </Typography>
-                {error ? <p style={{color: 'red'}}>{error}</p> : pwResetEmail ? <p style={{color: 'blue'}}>{pwResetEmail}</p> : null}
-                <form className={classes.form} noValidate method='post' onSubmit={handleResetPassword}>
+                {error ? (
+                    <p style={{ color: "red" }}>{error}</p>
+                ) : pwResetEmail ? (
+                    <p style={{ color: "blue" }}>{pwResetEmail}</p>
+                ) : null}
+                <form
+                    className={classes.form}
+                    noValidate
+                    method="post"
+                    onSubmit={handleResetPassword}
+                >
                     <TextField
-                        type='email'
-                        variant='outlined'
-                        margin='normal'
+                        type="email"
+                        variant="outlined"
+                        margin="normal"
                         required
                         fullWidth
-                        id='emailAddress'
-                        label='Email Address'
-                        autoComplete='emailAddress'
-                        name='emailAddress'
+                        id="emailAddress"
+                        label="Email Address"
+                        autoComplete="emailAddress"
+                        name="emailAddress"
                         value={emailAddress}
                         autoFocus
-                        onChange={({target}) => setEmailAddress(target.value)}
+                        onChange={({ target }) => setEmailAddress(target.value)}
                     />
                     <Button
                         disabled={disabled}
-                        type='submit'
+                        type="submit"
                         fullWidth
-                        variant='contained'
-                        color='primary'
+                        variant="contained"
+                        color="primary"
                         className={classes.submit}
                     >
                         PW Reset
@@ -119,5 +135,5 @@ export default function ForgotPassword() {
                 <Copyright />
             </Box>
         </Container>
-    )
+    );
 }
